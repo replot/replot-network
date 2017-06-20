f2d509fd1ba69d47baea0eb5563c4e02cb4d8662
Nancy Luong <nancyluong96@gmail.com>
2017-06-20 15:01:01 -0400

    compiling network

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,20 +1,22 @@
-module.exports = {
-  entry: {
-    example: "./example.jsx",
-  },
-  output: {
-    path: "static",
-    filename: "[name].bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: "babel-loader",
-        query: {
-          "presets": ["es2015", "react"]
+module.exports = function () {
+  return {
+    entry: {
+      example: "./example.jsx",
+    },
+    output: {
+      path: __dirname + "/static",
+      filename: "[name].bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx$/,
+          loader: "babel-loader",
+          query: {
+            "presets": ["es2015", "react"]
+          }
         }
-      }
-    ]
+      ]
+    }
   }
 };
