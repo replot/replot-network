4a404bdd53ec63e5e07e193345d9b6267eb1be4d
Nancy Luong <nancyluong96@gmail.com>
2017-08-15 13:00:59 -0400

    added motion to labels

--- examples/example.jsx
+++ examples/example.jsx
@@ -398,9 +398,13 @@ class ExampleApp extends React.Component {
         style={{textAlign:"center",fontFamily:"Open Sans"}}>
         <h1 style={{textAlign: "center"}}>Network Chart</h1>
         <div style={{width: "70%"}}>
-          <NetworkChart nodes={this.state.nodes1} links={this.state.links1}
-            labelKey="name" groupKey="group" nodeSize="off" nodeKey="nodeSize"
-            width="50%"/>
+          <NetworkChart
+            nodes={this.state.nodes2}
+            links={this.state.links2}
+            labelKey="id"
+            groupKey="group"
+            nodeSize="off"
+            nodeKey="nodeSize"/>
         </div>
       </div>
     )
