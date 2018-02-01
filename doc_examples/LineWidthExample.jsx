c6f6476b587ed40ea763dc95ff1e12620583b320
Alisa Ono <alisao@mit.edu>
2018-01-31 23:34:20 -0500

    New examples in treemap style

---
+++ doc_examples/LineWidthExample.jsx
@@ -0,0 +1,89 @@
+import React from "react"
+import NetworkChart from "../index.js"
+
+class LineWidthExample extends React.Component {
+
+  render() {
+    const trades = [
+      {exporter: "Germany", importer: "European Union", volume: 1468990},
+      {exporter: "Netherlands", importer: "European Union", volume: 798744},
+      {exporter: "European Union", importer: "France", volume: 745931},
+      {exporter: "European Union", importer: "United States", volume: 631868},
+      {exporter: "Canada", importer: "United States", volume: 615908},
+      {exporter: "Belgium", importer: "European Union", volume: 628796},
+      {exporter: "China", importer: "Hong Kong", volume: 496359},
+      {exporter: "China", importer: "United States", volume: 554573},
+      {exporter: "European Union", importer: "United Kingdom", volume: 580318},
+      {exporter: "China", importer: "European Union", volume: 547015},
+      {exporter: "Italy", importer: "European Union", volume: 539556},
+      {exporter: "Mexico", importer: "United States", volume: 501114},
+      {exporter: "Russia", importer: "European Union", volume: 395834},
+      {exporter: "Spain", importer: "European Union", volume: 365191},
+      {exporter: "China", importer: "Japan", volume: 311146},
+      {exporter: "European Union", importer: "Switzerland", volume: 300133},
+      {exporter: "South Korea", importer: "China", volume: 251471},
+      {exporter: "Poland", importer: "European Union", volume: 267854},
+      {exporter: "European Union", importer: "Austria", volume: 244913},
+      {exporter: "Czech Republic", importer: "European Union", volume: 225091},
+      {exporter: "Japan", importer: "United States", volume: 206464},
+      {exporter: "European Union", importer: "Sweden", volume: 204849},
+      {exporter: "China", importer: "Germany", volume: 187293},
+      {exporter: "Norway", importer: "European Union", volume: 179502},
+      {exporter: "Germany", importer: "United States", volume: 184247},
+      {exporter: "European Union", importer: "Turkey", volume: 158206},
+      {exporter: "Hungary", importer: "European Union", volume: 154862},
+      {exporter: "European Union", importer: "Japan", volume: 143807},
+      {exporter: "Australia", importer: "China", volume: 128725},
+      {exporter: "European Union", importer: "Denmark", volume: 129951},
+      {exporter: "Taiwan", importer: "China", volume: 124502},
+      {exporter: "United Kingdom", importer: "United States", volume: 116675},
+      {exporter: "Germany", importer: "Switzerland", volume: 115041},
+      {exporter: "Ireland", importer: "European Union", volume: 105853},
+    ]
+
+    const netExports = [
+      {region: "Europe", country: "Germany", exports: 1283000},
+      {region: "Europe", country: "European Union", exports: 2659000},
+      {region: "Europe", country: "Netherlands", exports: 460100},
+      {region: "Europe", country: "France", exports: 505400},
+      {region: "America", country: "United States", exports: 1471000},
+      {region: "America", country: "Canada", exports: 402400},
+      {region: "Europe", country: "Belgium", exports: 250800},
+      {region: "Asia", country: "China", exports: 2011000},
+      {region: "Asia", country: "Hong Kong", exports: 487700},
+      {region: "Europe", country: "United Kingdom", exports: 412100},
+      {region: "Europe", country: "Italy", exports: 436300},
+      {region: "America", country: "Mexico", exports: 359300},
+      {region: "Asia", country: "Russia", exports: 259300},
+      {region: "Europe", country: "Spain", exports: 266300},
+      {region: "Asia", country: "Japan", exports: 641400},
+      {region: "Europe", country: "Switzerland", exports: 301100},
+      {region: "Asia", country: "South Korea", exports: 509000},
+      {region: "Europe", country: "Poland", exports: 188300},
+      {region: "Europe", country: "Austria", exports: 142900},
+      {region: "Europe", country: "Czech Republic", exports: 131000},
+      {region: "Europe", country: "Sweden", exports: 151100},
+      {region: "Europe", country: "Norway", exports: 102900},
+      {region: "Asia", country: "Turkey", exports: 152000},
+      {region: "Europe", country: "Hungary", exports: 89440},
+      {region: "Oceania", country: "Australia", exports: 184300},
+      {region: "Europe", country: "Denmark", exports: 95970},
+      {region: "Asia", country: "Taiwan", exports: 314800},
+      {region: "Europe", country: "Ireland", exports: 125500},
+    ]
+
+    return(
+      <div>
+        <NetworkChart
+            data={trades}
+            parentKey="exporter"
+            childKey="importer"
+            lineWidth={5}
+        />
+      </div>
+    )
+  }
+}
+
+
+export default LineWidthExample
---
+++ doc_examples/examples.jsx
@@ -0,0 +1,121 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import BasicExample from "./BasicExample.jsx"
+import DimensionsExample from "./DimensionsExample.jsx"
+import LineColorExample from "./LineColorExample.jsx"
+import LineOpacityExample from "./LineOpacityExample.jsx"
+import LineWidthExample from "./LineWidthExample.jsx"
+import WeightedLinksExample from "./WeightedLinksExample.jsx"
+import MaxWidthExample from "./MaxWidthExample.jsx"
+import GroupDefaultExample from "./GroupDefaultExample.jsx"
+import CustomColorExample from "./CustomColorExample.jsx"
+import NodeRadiusExample from "./NodeRadiusExample.jsx"
+import WeightedNodesExample from "./WeightedNodesExample.jsx"
+import MaxRadiusExample from "./MaxRadiusExample.jsx"
+import ShowLabelsExample from "./ShowLabelsExample.jsx"
+import LabelColorExample from "./LabelColorExample.jsx"
+import LabelKeyExample from "./LabelKeyExample.jsx"
+import AttractionFactorExample from "./AttractionFactorExample.jsx"
+import TooltipDarkExample from "./TooltipDarkExample.jsx"
+import TooltipOffExample from "./TooltipOffExample.jsx"
+import TooltipCustomExample from "./TooltipCustomExample.jsx"
+
+ReactDOM.render(
+  <BasicExample />,
+  document.getElementById("basic-example")
+)
+
+// ReactDOM.render(
+//   <AdvancedExample />,
+//   document.getElementById("advanced-example")
+// )
+
+ReactDOM.render(
+  <DimensionsExample />,
+  document.getElementById("dimensions-example")
+)
+
+ReactDOM.render(
+  <LineColorExample />,
+  document.getElementById("line-color-example")
+)
+
+ReactDOM.render(
+  <LineOpacityExample />,
+  document.getElementById("line-opacity-example")
+)
+
+ReactDOM.render(
+  <LineWidthExample />,
+  document.getElementById("line-width-example")
+)
+
+ReactDOM.render(
+  <WeightedLinksExample />,
+  document.getElementById("weighted-links-example")
+)
+
+ReactDOM.render(
+  <MaxWidthExample />,
+  document.getElementById("max-width-example")
+)
+
+ReactDOM.render(
+  <GroupDefaultExample />,
+  document.getElementById("group-default-example")
+)
+
+ReactDOM.render(
+  <CustomColorExample />,
+  document.getElementById("custom-color-example")
+)
+
+ReactDOM.render(
+  <NodeRadiusExample />,
+  document.getElementById("node-radius-example")
+)
+
+ReactDOM.render(
+  <WeightedNodesExample />,
+  document.getElementById("weighted-nodes-example")
+)
+
+ReactDOM.render(
+  <MaxRadiusExample />,
+  document.getElementById("max-radius-example")
+)
+
+ReactDOM.render(
+  <ShowLabelsExample />,
+  document.getElementById("show-labels-example")
+)
+
+ReactDOM.render(
+  <LabelColorExample />,
+  document.getElementById("label-color-example")
+)
+
+ReactDOM.render(
+  <LabelKeyExample />,
+  document.getElementById("label-key-example")
+)
+
+ReactDOM.render(
+  <AttractionFactorExample />,
+  document.getElementById("attraction-factor-example")
+)
+
+ReactDOM.render(
+  <TooltipDarkExample />,
+  document.getElementById("tooltip-dark-example")
+)
+
+ReactDOM.render(
+  <TooltipOffExample />,
+  document.getElementById("tooltip-off-example")
+)
+
+ReactDOM.render(
+  <TooltipCustomExample />,
+  document.getElementById("tooltip-custom-example")
+)
